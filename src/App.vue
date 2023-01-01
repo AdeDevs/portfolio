eb90d7521cf3209b0bb088de31b961bf1e20e3de
Adeyemi Akinyemi <89089354+AdeDevs@users.noreply.github.com>
2023-01-01 21:28:12 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-container">
+    <MyHomePage />
+  </div>
+</template>
+
+<script>
+import MyHomePage from './components/MyHomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyHomePage
+  }
+}
+</script>
+
+<style scoped>
+@import "./styles/style.css";
+</style>
---
+++ src/components/MyTestPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>
+        Omorr why e no work
+    </h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
